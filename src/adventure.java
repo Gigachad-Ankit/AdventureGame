a5537eee4b9430bcf37e8b0c6bdd3203679e533a
felinatoren <niel765p@stud.kea.dk>
2021-09-30 13:15:42 +0200

    first commit

---
+++ src/adventure.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class adventure {
+  public static void main(String[] args) {
+    //scanner
+    Scanner input = new Scanner(System.in);
+
+    //moment
+    String north =
+
+
+    }
+  }
